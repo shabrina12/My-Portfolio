d40fd70ca0ebdcb800433e03d7b275b743e15831
Shabrina Putri <shabbrina5@gmail.com>
2024-01-09 14:58:54 +0700

    enhancements on about section

--- components/AboutSection.jsx
+++ components/AboutSection.jsx
@@ -2,19 +2,24 @@
 import React, { useTransition, useState } from 'react'
 import Image from 'next/image'
 import TabButton from './TabButton'
+import Link from 'next/link'
+import { Fraunces, Mulish } from 'next/font/google';
+
+const fraunces = Fraunces({subsets: ['latin']});
+const mulish = Mulish({subsets: ['latin']});
 
 const TAB_DATA = [
   {
     title: "Skills",
     id: "skills",
     content: (
-      <ul className='text-[#391400] list-disc pl-2'>
+      <ul style={mulish.style} className='text-[#391400] list-disc pl-2'>
         <li>HTML & CSS</li>
         <li>JavaScript</li>
         <li>NextJS</li>
         <li>ReactJS</li>
         <li>SQL</li>
-        <li>ASP.Net</li>
+        <li>C# ASP.Net</li>
       </ul>
     )
   },
@@ -22,7 +27,7 @@ const TAB_DATA = [
     title: "Education",
     id: "education",
     content: (
-      <ul className='text-[#391400]'>
+      <ul style={mulish.style} className='text-[#391400]'>
         <li>Bachelor of Computer Engineering at Institut Teknologi Sepuluh Nopember, Indonesia</li>
       </ul>
     )
@@ -31,11 +36,11 @@ const TAB_DATA = [
     title: "Certifications",
     id: "certifications",
     content: (
-      <ul className='text-[#391400] list-disc pl-2'>
-        <li>PL-900: Microsoft Power Platform Fundamentals</li>
-        <li>Full Stack Developer by Metrodata Academy</li>
-        <li>Back End Developer by Fresh Graduate Academy Digitalent</li>
-        <li>Cloud Computing by Bangkit Academy 2021</li>
+      <ul style={mulish.style} className='text-[#391400] list-disc pl-2'>
+        <li className='hover:cursor-pointer hover:text-[#EF6D58]'><Link href="https://drive.google.com/file/d/1612Qr6f7W3U7kC715v4vFbdkv0Gh4KGs/view" target="_blank">PL-900: Microsoft Power Platform Fundamentals</Link></li>
+        <li className='hover:cursor-pointer hover:text-[#EF6D58]'><Link href="https://drive.google.com/file/d/1yIjL0lG8RIAuiBpM80moaiLQtrU-viEK/view?usp=sharing" target="_blank">Full Stack Developer by Metrodata Academy</Link></li>
+        <li className='hover:cursor-pointer hover:text-[#EF6D58]'><Link href="https://drive.google.com/file/d/13pxxUM2QOxaiYzbhnGo-uea9f7q17L45/view?usp=sharing" target="_blank">Back End Developer by Fresh Graduate Academy Digitalent</Link></li>
+        <li className='hover:cursor-pointer hover:text-[#EF6D58]'><Link href="https://drive.google.com/file/d/1igLHEKlEam9IJkr-hGYWRmhxUymVGsnL/view?usp=sharing" target="_blank">Cloud Computing by Bangkit Academy 2021</Link></li>
       </ul>
     )
   }
@@ -59,9 +64,11 @@ const AboutSection = () => {
           <Image className='absolute top-[75%] left-[-3%]' src="/profile-circle.png" width={150} height={150} />
         </div>
         <div className='h-full'>
-          <h4 className='text-[#EF6D58] sm:mt-4 mb-3'>SHABRINA PUTRI</h4>
-          <h2 className='text-4xl text-[#391400] mb-3 font-bold'>Junior Full Stack<br/> Developer</h2>
-          <p className='text-[#391400] md:text-lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <h4 className='text-[#EF6D58] sm:mt-4 mb-3'>ABOUT</h4>
+          <h2 style={fraunces.style} className='text-4xl text-[#391400] mb-3 font-bold'>Junior Full Stack<br/> Developer</h2>
+          <p style={mulish.style} className='text-[#391400] md:text-lg text-left'>
+          I'm well-versed in a variety of technologies, including but not limited to HTML, CSS, JavaScript, and popular frameworks like NextJS and React on the front end. On the server side, I've worked with ASP.Net and Node.js, and my database skills include SQL database.
+          </p>
           <div className='flex flex-row mt-8'>
             <TabButton selectTab={() => handleTabChange("skills")} active={tab === "skills"}>
               {" "}
